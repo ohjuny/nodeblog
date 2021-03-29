e3a62520d708861609baf1c6f06d11ac994fdf5c
ohjun <ohjun@umich.edu>
2021-03-29 12:12:21 +0400

    Created header and footer EJS files for repeated code

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@ router.get('/', function(req, res) {
   var collection = db.get('usercollection');
   collection.find({},{},function(e, docs) {
     res.render('users', {
-      'title': 'Node Blog',
       'users': docs
     });
   });
